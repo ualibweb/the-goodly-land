e26326ba146dd0818b5e2cb609981993e1b5060a
Jared Cleghorn <jrcleghorn@crimson.ua.edu>
2019-04-16 12:31:14 -0500

    Add front page template

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The front page template.
+ */
+
+get_header();
+
+if ( have_posts() ) :
+  while ( have_posts() ) :
+    the_post();
+    the_content();
+  endwhile;
+endif;
+
+get_footer();
+?>
