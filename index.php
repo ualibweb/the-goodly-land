1f53c149443cf68358007a35531275f203746c05
Jared Cleghorn <jrcleghorn@crimson.ua.edu>
2019-04-29 07:43:58 -0500

    Fix title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ get_header();
 if (have_posts()) :
   while (have_posts()) :
     the_post();
-    echo '<h2>' . the_title() . '</h2>';
+    the_title('<h2>', '</h2>');
     the_content();
   endwhile;
 endif;
